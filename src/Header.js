77420120179756da7db80bad55bd530e863e9e2a
sarojrani <sarojkumarirani345@gmail.com>
2023-06-03 15:13:41 +0530

    updated

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ const Header=()=>{
          <div>
     
         <ul className="flex flex-row  justify-around ">
-            <li className=" pr-10  mr-5 self-center  font-mono font-bold text-3xl">Job Post Manger For Website</li>
+            <li className=" pr-10  mr-5 self-center  font-mono font-bold text-3xl">Job Post Manager For Website</li>
             <div className="justify-end m-5">
             <li className="   text-xl font-semibold font-mono text-ml self-end ">Teck Task For <span className="text-[#c56b11] font-bold text-xl">Handy</span>Solver</li>
             
